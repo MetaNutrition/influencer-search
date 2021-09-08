98b2c7ca11f2312ff98b225875d01c125cfa82bc
Rand Hale <randluca@gmail.com>
2021-09-08 10:46:58 -0700

    move Button component from pages/index.js to unique component file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import styled from 'styled-components'
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import styled from 'styled-components'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Button from '../components/Button'
 
 const Container = styled.div`
   min-height: 100vh;
@@ -16,21 +18,6 @@ const Main = styled.div`
   flex: 1;
   flex-direction: column;
   text-align: center;
-`
-const Button = styled.button`
-  background: transparent;
-  border-radius: 5px;
-  border: 2px solid black;
-  color: black;
-  margin: 1rem;
-  padding: 1rem;
-  width: 200px;
-  height: 60px;
-  cursor: pointer;
-  font-weight: 600;
-  font-size: 1em;
-  background: ${props => props.primary ? "black" : "transparent"};
-  color: ${props => props.primary ? "white" : "black"};
 `
 const Title = styled.h1`
   line-height: .5;
@@ -66,54 +53,3 @@ const Home = () => {
 }
 
 export default Home;
-
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-// <Head>
-//   <title>MetNu Influencer Search</title>
-//   <meta name="description" content="Powered by NextJS and React" />
-//   <link rel="icon" href="/favicon.ico" />
-// </Head>
-
-      // <header className={styles.header}>
-      //   <ul>
-      //     <li>
-      //       Search
-      //     </li>
-      //     <li>
-      //       Saved
-      //     </li>
-      //     <li>
-      //       About
-      //     </li>
-      //   </ul>
-      // </header>
-
-//       <main className={styles.main}>
-
-//         <h1 className={styles.title}>
-//           TikTok Influencer Search
-//         </h1>
-
-//         <p className={styles.subtitle}>
-//           MetNu Internal Tool
-//         </p>
-
-//         <p className={styles.startBox}>Get Started</p>
-
-//       </main>
-
-//       <footer className={styles.footer}>
-        // <a
-        //   href="https://www.metnu.com/"
-        //   target="_blank"
-        //   rel="noopener noreferrer"
-        // >
-        //   MetNu Home
-        // </a>
-
-//       </footer>
-//     </div>
-//   )
-// }
