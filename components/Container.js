0b3aca300f1ade779165f7378e2cc8f7b2004713
Rand Hale <randluca@gmail.com>
2021-09-09 10:54:42 -0700

    separate Container and Main components into unique files for use in subsequent pages

---
+++ components/Container.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+export default Container;
---
+++ components/Main.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Main = styled.div`
+  padding: 2rem 0;
+  flex: 1;
+  flex-direction: column;
+  text-align: center;
+`
+
+export default Main;
